e56928aaa5bd7693a3ddf0ddd31f78074476188e
Ayush <aayush199813@gmail.com>
2022-08-03 20:36:23 +0530

    Style of task

---
+++ Resourses/js/index.js
@@ -0,0 +1,9 @@
+
+// User clicked on Add button
+// if there is an item in text then add to the todo list
+document.getElementById('add').addEventListener('click', function () {
+     var value = document.getElementById('item').value;
+     if (value) {
+         
+    }
+})
