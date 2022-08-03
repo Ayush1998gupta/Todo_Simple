769474ab413355185ce3383bac47d0f7e06ff3fb
Ayush <aayush199813@gmail.com>
2022-08-04 00:34:58 +0530

    Complete task Button

--- Resourses/js/index.js
+++ Resourses/js/index.js
@@ -9,11 +9,32 @@ document.getElementById('add').addEventListener('click', function () {
 });
 
 function removeItem() {
-     var item =this.parentNode.parentNode
-     var parent = item.parentNode;
-     parent.removeChild(item);
-     
+  var item = this.parentNode.parentNode;
+  var parent = item.parentNode;
+  parent.removeChild(item);
 }
+
+function completeItem() {
+  var item = this.parentNode.parentNode;
+  var parent = item.parentNode;
+  var id = parent.id;
+
+  var target =
+    id === 'todo'
+      ? document.getElementById('completed')
+      : document.getElementById('todo');
+
+  parent.removeChild(item);
+  target.insertBefore(item, target.childNodes[0]);
+
+  
+  if (item.parentNode.id === 'completed') {
+    this.innerHTML = ' <i class="fa-solid fa-circle-check"></i>';
+  } else {
+    this.innerHTML = ' <i class="fa-regular fa-circle-check"></i>';
+  }
+}
+
 // Adds a new item to the todo list
 function addItemTodo(text) {
   var list = document.getElementById('todo');
@@ -33,6 +54,9 @@ function addItemTodo(text) {
   var complete = document.createElement('button');
   complete.classList.add('complete');
   complete.innerHTML = '<i class="fa-regular fa-circle-check"></i>';
+
+  // Add click event for complete items
+  complete.addEventListener('click', completeItem);
 
   buttons.appendChild(remove);
   buttons.appendChild(complete);
