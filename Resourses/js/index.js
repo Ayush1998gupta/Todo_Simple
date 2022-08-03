7e069d07655b28bf7dbbe6ef2f2dbe7a61615029
Ayush <aayush199813@gmail.com>
2022-08-04 01:41:30 +0530

    Added count

--- Resourses/js/index.js
+++ Resourses/js/index.js
@@ -1,23 +1,47 @@
+var data = {
+  todo: [],
+  complete: [],
+};
 // User clicked on Add button
 // if there is an item in text then add to the todo list
 document.getElementById('add').addEventListener('click', function () {
   var value = document.getElementById('item').value;
   if (value) {
     addItemTodo(value);
+
     document.getElementById('item').value = '';
+    data.todo.push(value);
+    itemLeft();
   }
 });
 
 function removeItem() {
   var item = this.parentNode.parentNode;
   var parent = item.parentNode;
+  var id = parent.id;
+  var value = item.innerText;
+  if (id === 'todo') {
+    data.todo.splice(data.todo.indexOf(value), 1);
+  } else {
+    data.complete.splice(data.complete.indexOf(value), 1);
+  }
   parent.removeChild(item);
+  itemLeft();
 }
 
 function completeItem() {
   var item = this.parentNode.parentNode;
   var parent = item.parentNode;
   var id = parent.id;
+  var value = item.innerText;
+
+  if (id === 'todo') {
+    data.todo.splice(data.todo.indexOf(value), 1);
+    data.complete.push(value);
+  } else {
+    data.complete.splice(data.complete.indexOf(value), 1);
+    data.todo.push(value);
+  }
 
   var target =
     id === 'todo'
@@ -27,12 +51,13 @@ function completeItem() {
   parent.removeChild(item);
   target.insertBefore(item, target.childNodes[0]);
 
-  
   if (item.parentNode.id === 'completed') {
     this.innerHTML = ' <i class="fa-solid fa-circle-check"></i>';
   } else {
     this.innerHTML = ' <i class="fa-regular fa-circle-check"></i>';
   }
+
+  itemLeft();
 }
 
 // Adds a new item to the todo list
@@ -62,4 +87,8 @@ function addItemTodo(text) {
   buttons.appendChild(complete);
   item.appendChild(buttons);
   list.insertBefore(item, list.childNodes[0]);
+}
+
+function itemLeft() {
+   document.querySelector('h3').innerText= `${data.todo.length} Tasks left`
 }
